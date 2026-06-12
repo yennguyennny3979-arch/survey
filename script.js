604a2868d29d7768a2a9bb48ac6494d8227c99e0
yennguyennny3979-arch <yennguyen.nny3979@gmail.com>
2026-06-12 09:47:19 +0700

    Update script.js
    const questions = [
    
    {
    title:"Câu 1",
    images:[
    "https://picsum.photos/300?1",
    "https://picsum.photos/300?2",
    "https://picsum.photos/300?3",
    "https://picsum.photos/300?4"
    ]
    },
    
    {
    title:"Câu 2",
    images:[
    "https://picsum.photos/300?5",
    "https://picsum.photos/300?6",
    "https://picsum.photos/300?7",
    "https://picsum.photos/300?8"
    ]
    },
    
    {
    title:"Câu 3",
    images:[
    "https://picsum.photos/300?9",
    "https://picsum.photos/300?10",
    "https://picsum.photos/300?11",
    "https://picsum.photos/300?12"
    ]
    }
    
    ];
    
    let currentQuestion = 0;
    
    function loadQuestion(){
    
    if(currentQuestion >= questions.length){
    
    document.body.innerHTML =
    "<h1>Cảm ơn bạn đã tham gia khảo sát!</h1>";
    
    return;
    }
    
    const q = questions[currentQuestion];
    
    document.querySelector("h1").innerText =
    `${q.title} (${currentQuestion + 1}/${questions.length})`;
    
    const images =
    document.querySelectorAll("img");
    
    q.images.forEach((url,index)=>{
    images[index].src = url;
    });
    
    startTimer();
    
    }
    
    function startTimer(){
    
    let time = 5;
    
    const timer =
    document.querySelector("h2");
    
    timer.innerText = "⏳ " + time;
    
    const interval =
    setInterval(()=>{
    
    time--;
    
    timer.innerText = "⏳ " + time;
    
    if(time <= 0){
    
    clearInterval(interval);
    
    currentQuestion++;
    
    loadQuestion();
    
    }
    
    },1000);
    
    }
    
    loadQuestion();

--- script.js
+++ script.js
@@ -1,19 +1,93 @@
+const questions = [
+
+{
+title:"Câu 1",
+images:[
+"https://picsum.photos/300?1",
+"https://picsum.photos/300?2",
+"https://picsum.photos/300?3",
+"https://picsum.photos/300?4"
+]
+},
+
+{
+title:"Câu 2",
+images:[
+"https://picsum.photos/300?5",
+"https://picsum.photos/300?6",
+"https://picsum.photos/300?7",
+"https://picsum.photos/300?8"
+]
+},
+
+{
+title:"Câu 3",
+images:[
+"https://picsum.photos/300?9",
+"https://picsum.photos/300?10",
+"https://picsum.photos/300?11",
+"https://picsum.photos/300?12"
+]
+}
+
+];
+
+let currentQuestion = 0;
+
+function loadQuestion(){
+
+if(currentQuestion >= questions.length){
+
+document.body.innerHTML =
+"<h1>Cảm ơn bạn đã tham gia khảo sát!</h1>";
+
+return;
+}
+
+const q = questions[currentQuestion];
+
+document.querySelector("h1").innerText =
+`${q.title} (${currentQuestion + 1}/${questions.length})`;
+
+const images =
+document.querySelectorAll("img");
+
+q.images.forEach((url,index)=>{
+images[index].src = url;
+});
+
+startTimer();
+
+}
+
+function startTimer(){
+
 let time = 5;
 
-const timer = document.querySelector("h2");
+const timer =
+document.querySelector("h2");
 
-setInterval(() => {
+timer.innerText = "⏳ " + time;
 
-    time--;
+const interval =
+setInterval(()=>{
 
-    timer.innerText = "⏳ " + time;
+time--;
 
-    if(time <= 0){
+timer.innerText = "⏳ " + time;
 
-        alert("Hết giờ!");
+if(time <= 0){
 
-        time = 5;
+clearInterval(interval);
 
-    }
+currentQuestion++;
+
+loadQuestion();
+
+}
 
 },1000);
+
+}
+
+loadQuestion();
