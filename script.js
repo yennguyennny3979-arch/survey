a972d8bc16fce557a57a8a6481233a2f4660f746
yennguyennny3979-arch <yennguyen.nny3979@gmail.com>
2026-06-12 09:33:29 +0700

    Update script.js
    let time = 5;
    
    const timer = document.querySelector("h2");
    
    setInterval(() => {
    
        time--;
    
        timer.innerText = "⏳ " + time;
    
        if(time <= 0){
    
            alert("Hết giờ!");
    
            time = 5;
    
        }
    
    },1000);

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Khảo sát");
+let time = 5;
+
+const timer = document.querySelector("h2");
+
+setInterval(() => {
+
+    time--;
+
+    timer.innerText = "⏳ " + time;
+
+    if(time <= 0){
+
+        alert("Hết giờ!");
+
+        time = 5;
+
+    }
+
+},1000);
